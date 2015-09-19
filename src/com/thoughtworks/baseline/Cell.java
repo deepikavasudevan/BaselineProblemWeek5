82ddb19a1889eb92d22a7636aff7630d197accea
deepika <deepikav2793@gmail.com>
2015-09-19 12:37:13 +0530

    Created spec for checking bottom neighbour of cell. Incremented number accordingly

--- src/com/thoughtworks/baseline/Cell.java
+++ src/com/thoughtworks/baseline/Cell.java
@@ -42,9 +42,12 @@ public class Cell {
         if (seed[xPosition][yPosition + 1].state.equals("X"))
             numberOfLiveCellNeighbours++;
 
-        if(seed[xPosition + 1][yPosition - 1].state.equals("X"))
+        if (seed[xPosition + 1][yPosition - 1].state.equals("X"))
             numberOfLiveCellNeighbours++;
 
-            return numberOfLiveCellNeighbours;
+        if (seed[xPosition + 1][yPosition].state.equals("X"))
+            numberOfLiveCellNeighbours++;
+
+        return numberOfLiveCellNeighbours;
     }
 }
